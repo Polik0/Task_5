7558aff419348aa667b6c597239f2b44be2b4207
Polik0 <ppolina0404@gmail.com>
2022-05-20 11:13:33 +0200

    Task 5

---
+++ task_5.js
@@ -0,0 +1,38 @@
+// Task 1
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i + 1);
+// }
+
+// Task 2
+
+// function randomInteger(min, max) {
+//   let rand = min + Math.random() * (max + 1 - min);
+//   return Math.floor(rand);
+// }
+
+// const number = randomInteger(1, 10);
+
+// if (number % 2 == 0) {
+//   console.log("Число ", number, "парне");
+// } else {
+//   console.log("Число ", number, "непарне");
+// }
+
+// Task 3
+
+// let num = 20;
+
+// while (num <= 28) {
+//   console.log(num + 1);
+//   num++;
+// }
+
+// Task 4
+
+// do {
+//   let name = prompt("Введіть ім'я свого улюбленого героя");
+//   l = name.length;
+//   if (l >= 6) break;
+//   console.log(name);
+// } while (l < 6);
